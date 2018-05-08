f5ca9f37c7c0a5cf1b58b5c87a6231c70125784b
yuyifang <yu.yf@foxmail.com>
2018-05-08 14:19:22 +0800

    1122

--- src/test/java/com/eyun258/jmstest/AppTest.java
+++ src/test/java/com/eyun258/jmstest/AppTest.java
@@ -13,7 +13,7 @@ public class AppTest
     /**
      * Create the test case   
      *
-     * @param testName name of the test case   DDD
+     * @param testName name of the test case   DDD  ccc
      * 
      * master 01
      * master 02
